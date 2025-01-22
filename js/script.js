c1c77e26e1d7791c54b3dec7fd13693a0e1f3d19
Hache94k <hernansolis94k@gmail.com>
2025-01-22 14:11:58 -0300

    version 16.3 fomrspree ok cloud

--- js/script.js
+++ js/script.js
@@ -131,4 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     return regex.test(email);
   }
+
+  
+
 });
